84d1b8d39e6560da94f48d93c6e197005fc456ff
emsdev23 <129821095+emsdev23@users.noreply.github.com>
2024-01-30 19:26:18 +0530

    Add files via upload

--- src/ipAdress.js
+++ src/ipAdress.js
@@ -1,1 +1,4 @@
-export const ipAddress = "43.205.196.66";
+export const dashboardAddress = "https://ems.tre100.in:443/dashboard/";
+export const analyticsAdress= "https://ems.tre100.in:443/analytics/"
+export const bmssAdress= "https://ems.tre100.in:443/bms/"
+export const nodeAdress= "https://ems.tre100.in:443/node/"
